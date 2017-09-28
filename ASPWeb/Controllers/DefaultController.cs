bd6e43d382727afbb148928408c7667bda6f1b67
Rafał Michalczewski <radmin@eleclerc.local>
2017-09-28 16:20:58 +0200

    Dodaj pliki projektów.

---
+++ ASPWeb/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPWeb.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult one()
+        {
+            return View();
+        }
+
+		public string Text()
+		{
+			return "Raz";
+		}
+    }
+}
